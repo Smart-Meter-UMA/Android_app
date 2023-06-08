12c48b4d777cbb4ebf603121c9df7211764ff69f
Raúl Salas <raulsalas.martin@gmail.com>
2023-06-08 13:30:34 +0200

    Corrigiendo Timer
    Ya no se cierra sesión al iniciar la app

--- app/src/main/java/es/uma/smartmeter/TimerActivity.java
+++ app/src/main/java/es/uma/smartmeter/TimerActivity.java
@@ -5,11 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -25,14 +20,14 @@ public class TimerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("523829369104-ea6e5875hq438cmfrfvffiq58cm43oia.apps.googleusercontent.com")
+        /*GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         if (account != null) mGoogleSignInClient.signOut();
-        System.out.println("Logout");
+        System.out.println("Logout");*/
     }
 
     @Override
